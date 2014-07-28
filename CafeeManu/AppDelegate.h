67754cec6c1a3cdb852edc58e5733c6b170d9760
arthopar <artaktsatinyan@gmail.com>
2014-07-28 21:05:23 +0400

    Created project

---
+++ CafeeManu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CafeeManu
+//
+//  Created by Artak on 7/28/14.
+//  Copyright (c) 2014 Artak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CafeeManu/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CafeeManu
+//
+//  Created by Artak on 7/28/14.
+//  Copyright (c) 2014 Artak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
