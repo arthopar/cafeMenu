1c5fd0290bd87dd11594fa333ff2947b165259cc
Tigran <tigran@pps-MacBook-Pro.local>
2014-07-30 01:42:12 +0400

    first page: finally done

---
+++ CafeeManu/MainViewController.h
@@ -0,0 +1,20 @@
+//
+//  MainViewController.h
+//  CafeeManu
+//
+//  Created by Tigran on 7/28/14.
+//  Copyright (c) 2014 Artak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface MainViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *btnLanguage;
+@property (weak, nonatomic) IBOutlet UIButton *btnMenu;
+@property (weak, nonatomic) IBOutlet UIButton *btnFeedback;
+@property (weak, nonatomic) IBOutlet UIButton *btnInformation;
+
+
+@end
---
+++ CafeeManu/MainViewController.m
@@ -0,0 +1,78 @@
+//
+//  MainViewController.m
+//  CafeeManu
+//
+//  Created by Tigran on 7/28/14.
+//  Copyright (c) 2014 Artak. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+    // Background
+    UIGraphicsBeginImageContext(self.view.frame.size);
+    [[UIImage imageNamed:@"background.jpg"] drawInRect:self.view.bounds];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    self.view.backgroundColor = [UIColor colorWithPatternImage:image];
+    
+    // Round buttons
+    CALayer * l = [_btnMenu layer];
+    [l setMasksToBounds:YES];
+    [l setCornerRadius:75.0];
+    [l setBorderWidth:2.0];
+    
+    l = [_btnInformation layer];
+    [l setMasksToBounds:YES];
+    [l setCornerRadius:50.0];
+    [l setBorderWidth:2.0];
+    
+    l = [_btnLanguage layer];
+    [l setMasksToBounds:YES];
+    [l setCornerRadius:50.0];
+    [l setBorderWidth:2.0];
+
+    l = [_btnFeedback layer];
+    [l setMasksToBounds:YES];
+    [l setCornerRadius:50.0];
+    [l setBorderWidth:2.0];
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
