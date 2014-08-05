23dbb11240ad914d7c9cdfd467d0ed9d83382a20
arthopar <artaktsatinyan@gmail.com>
2014-08-05 23:03:55 +0400

    Added localization support

---
+++ CafeeManu/Localization.h
@@ -0,0 +1,13 @@
+//
+//  Localization.h
+//  CafeeManu
+//
+//  Created by Artak on 8/5/14.
+//  Copyright (c) 2014 Artak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Localization : NSObject
+
+@end
---
+++ CafeeManu/Localization.m
@@ -0,0 +1,13 @@
+//
+//  Localization.m
+//  CafeeManu
+//
+//  Created by Artak on 8/5/14.
+//  Copyright (c) 2014 Artak. All rights reserved.
+//
+
+#import "Localization.h"
+
+@implementation Localization
+
+@end
